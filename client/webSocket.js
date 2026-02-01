3749e863cf06aadcebfd05c84a13803c6f52d732
satishreddy955 <168215588+satishreddy955@users.noreply.github.com>
2026-02-01 12:12:12 +0530

    Update webSocket.js

--- client/webSocket.js
+++ client/webSocket.js
@@ -1,5 +1,5 @@
 export function initSocket(handlers){
-    const socket = io("http://localhost:3000");
+    const socket = io("https://canvasassign.onrender.com");
     socket.on("init",handlers.onInit);
     socket.on("stroke:commit",handlers.onStrokeCommit);
     socket.on("state:update",handlers.onCursorUpdate);
